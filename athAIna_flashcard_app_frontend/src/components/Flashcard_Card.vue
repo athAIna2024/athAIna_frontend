86d5685a5052306aa2e88bdfcc1bb699c07c4497
Myrandde <115961838+Myrandde@users.noreply.github.com>
2024-11-03 02:46:27 +0800

    added searchbar and library page flashcard

--- athAIna_flashcard_app_frontend/src/components/Flashcard_Card.vue
+++ athAIna_flashcard_app_frontend/src/components/Flashcard_Card.vue
@@ -5,12 +5,13 @@ export default {
 </script>
 
 <template>
-  <div class="">
-    Flashcard card
-    It includes the edit and delete button,
-    you can reference here the modals
-    (Update_Flashcard and Delete_Flashcard)
-  </div>
+  <button class="border-athAIna-red border-4 rounded-lg m-4 p-6 py-16 w-96" @click="goFlashCard(flashcard.id)">
+    <h1 class="">{{ question }}</h1>
+    <div class="border border-athAIna-red border-t-2 border-0 mt-8 flex justify-end">
+      <button @click="editFlashCard(flashcard)" class="p-2">Edit</button>
+      <button @click="deleteFlashCard(flashcard)" class="p-2">Delete</button>
+    </div>
+  </button>
 </template>
 
 <style scoped>
