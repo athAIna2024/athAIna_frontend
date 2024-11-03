7e96cd9a005ace743b499c97e8050d79cb03eeac
Myrandde <115961838+Myrandde@users.noreply.github.com>
2024-11-03 20:56:16 +0800

    added searchbar func, library page

--- athAIna_flashcard_app_frontend/src/components/Flashcard_Card.vue
+++ athAIna_flashcard_app_frontend/src/components/Flashcard_Card.vue
@@ -8,11 +8,11 @@ export default {
 </script>
 
 <template>
-  <button class="border-athAIna-red border-4 rounded-lg m-4 p-6 py-16 w-96" @click="goFlashCard(flashcard.id)">
+  <button class="border-athAIna-red border-4 rounded-lg m-4 p-6 py-16 w-96" @click="goFlashCard()">
     <h1 class="">{{ flashcard.question }}</h1>
-    <div class="border border-athAIna-red border-t-2 border-0 mt-8 flex justify-end">
-      <button @click="editFlashCard(flashcard)" class="p-2">Edit</button>
-      <button @click="deleteFlashCard(flashcard)" class="p-2">Delete</button>
+    <div class="border-athAIna-red border-t-2 border-0 mt-8 flex justify-end">
+      <button class="p-2">Edit</button>
+      <button class="p-2">Delete</button>
     </div>
   </button>
 </template>
