b08922db0622e85a1159fb2424b784502df59b0a
— <alekzene.cs@gmail.com>
2024-11-12 10:41:18 +0800

    change page title to 'Study Sets | athAIna'

--- athAIna_flashcard_app_frontend/src/views/studysetapp/Library_Page_Studyset.vue
+++ athAIna_flashcard_app_frontend/src/views/studysetapp/Library_Page_Studyset.vue
@@ -2,9 +2,13 @@
 import Studyset_Search_Bar from '@/components/Studyset_Search_Bar.vue';
 import Studyset_Card from "@/components/Studyset_Card.vue";
 import Subject_Selector from "@/components/Subject_Selector.vue";
+
 export default {
   name: 'Library_Page_Studyset',
   components: {Subject_Selector, Studyset_Search_Bar, Studyset_Card},
+  mounted() {
+    document.title = `Study Sets – athAIna`
+  }
 };
 </script>
 
