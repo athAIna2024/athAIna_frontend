759535c59b2579856ba69b997e752f9ec3e41760
— <alekzene.cs@gmail.com>
2024-11-16 17:48:08 +0800

    comp: update subject selector

--- athAIna_flashcard_app_frontend/src/components/Subject_Selector.vue
+++ athAIna_flashcard_app_frontend/src/components/Subject_Selector.vue
@@ -5,11 +5,16 @@ export default {
 </script>
 
 <template>
-  <div class="">
-    Subject selector for studyset
-    Refer to 4.13 in Figma
+  <div class="bg-gradient-to-br from-athAIna-yellow via-athAIna-orange to-athAIna-red rounded-[20px] h-[40px] flex justify-center items-center">
+    <div class="flex flex-row text-[14px] text-athAIna-orange w-[400px] justify-center items-center bg-athAIna-white rounded-[25px] ml-[4px] mr-[4px] p-[5.5px]">
+      <span>Technology</span>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5 text-athAIna-violet">
+        <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+      </svg>
+    </div>
   </div>
 </template>
+
 
 <style scoped>
 </style>
