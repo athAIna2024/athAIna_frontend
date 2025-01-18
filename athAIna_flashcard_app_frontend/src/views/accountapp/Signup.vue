6d9641fead45be6b74fb4fec445adf997e8b61fd
netzumin <115604150+3aarl@users.noreply.github.com>
2025-01-18 15:45:26 +0800

    (UNTESTED) Base Integration Update
    future me, with my dear pc set up, test this with the back end

--- athAIna_flashcard_app_frontend/src/views/accountapp/Signup.vue
+++ athAIna_flashcard_app_frontend/src/views/accountapp/Signup.vue
@@ -1,7 +1,36 @@
-<script>
-export default {
-  name: 'Signup',
-};
+<script setup>
+import {ref} from 'vue'
+import {useRouter} from 'vue-router'
+import axios from 'axios'
+
+const router = useRouter()
+
+const email = ref('')
+const password = ref('')
+const confirmPassword = ref('')
+
+const error = ref('')
+
+const handleSignup = asynch () => {
+  if (password.value !== confirmPassword.value){
+    error.value = 'Passwords do not match'
+    return
+  }
+  try {
+    const response = await axios.post('http://localhost:8000/account/register/',{}
+    email: email.value,
+    password: password.value,
+    confirmPassword: confirmPassword.value
+  })
+  if (response.data){
+    router.push('/login')
+  } 
+  } catch (err) {
+    error.value = err.response.data.detail
+  }
+}
+
+
 </script>
 
 <template>
@@ -18,7 +47,7 @@ export default {
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="size-5 absolute left-3 top-1/2 transform -translate-y-1/2 text-athAIna-violet ml-2 mr-3">
                <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
               </svg>
-              <input type="text" placeholder="Email" class="text-[14px] text-athAIna-violet placeholder-athAIna-violet focus: outline-none ring- ring-athAIna-yellow w-full rounded-[15px] m-[4px] h-[32px] flex flex-row items-center pl-[50px]" />
+              <input v-model=email" type="email" placeholder="Email" class="text-[14px] text-athAIna-violet placeholder-athAIna-violet focus: outline-none ring- ring-athAIna-yellow w-full rounded-[15px] m-[4px] h-[32px] flex flex-row items-center pl-[50px]" />
           </div>
        </div>
 
@@ -28,7 +57,7 @@ export default {
             <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
             <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
           </svg>
-              <input type="text" placeholder="Password" class="text-[14px] text-athAIna-violet placeholder-athAIna-violet focus: outline-none ring- ring-athAIna-yellow w-full rounded-[15px] m-[4px] h-[32px] flex flex-row items-center pl-[50px]" />
+              <input v-model="password" type="password" placeholder="Password" class="text-[14px] text-athAIna-violet placeholder-athAIna-violet focus: outline-none ring- ring-athAIna-yellow w-full rounded-[15px] m-[4px] h-[32px] flex flex-row items-center pl-[50px]" />
           </div>
        </div>
        
@@ -38,9 +67,14 @@ export default {
             <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
             <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
           </svg>
-          <input type="text" placeholder="Confirm Password" class="text-[14px] text-athAIna-violet placeholder-athAIna-violet focus: outline-none ring- ring-athAIna-yellow w-full rounded-[15px] m-[4px] h-[32px] flex flex-row items-center pl-[50px]" />
+          <input v-model="confirmPassword" type="password" placeholder="Confirm Password" class="text-[14px] text-athAIna-violet placeholder-athAIna-violet focus: outline-none ring- ring-athAIna-yellow w-full rounded-[15px] m-[4px] h-[32px] flex flex-row items-center pl-[50px]" />
         </div>
         
+          <!-- Display error message if any -->
+        <div v-if="error" class="text-athAIna-red text-center mt-2">
+          {{ error }}
+        </div>
+
         <div class="flex m-10 justify-center">
           <button class="btn w-full"> 
             Sign Up
