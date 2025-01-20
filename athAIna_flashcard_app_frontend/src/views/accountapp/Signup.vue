74db4ee1de973d369df8b343c20eaca359a98755
netzumin <115604150+3aarl@users.noreply.github.com>
2025-01-20 12:06:24 +0800

    Signup Post Integrated

--- athAIna_flashcard_app_frontend/src/views/accountapp/Signup.vue
+++ athAIna_flashcard_app_frontend/src/views/accountapp/Signup.vue
@@ -1,94 +1,160 @@
 <script setup>
-import {ref} from 'vue'
-import {useRouter} from 'vue-router'
-import axios from 'axios'
+import { ref } from "vue";
+import axios from "axios";
 
-const router = useRouter()
+const email = ref("");
+const password = ref("");
+const password2 = ref("");
+const error = ref("");
 
-const email = ref('')
-const password = ref('')
-const confirmPassword = ref('')
-
-const error = ref('')
-
-const handleSignup = asynch () => {
-  if (password.value !== confirmPassword.value){
-    error.value = 'Passwords do not match'
-    return
+const createUser = async () => {
+  if (password.value !== password2.value) {
+    error.value = "Passwords do not match";
+    return;
   }
+
   try {
-    const response = await axios.post('http://localhost:8000/account/register/',{}
-    email: email.value,
-    password: password.value,
-    confirmPassword: confirmPassword.value
-  })
-  if (response.data){
-    router.push('/login')
-  } 
+    const response = await axios.post(
+      "http://localhost:8000/account/register/",
+      {
+        email: email.value,
+        password: password.value,
+        password2: password2.value,
+      }
+    );
+    console.log(response.data);
   } catch (err) {
-    error.value = err.response.data.detail
+    error.value = err.response.data.detail;
   }
-}
-
-
+};
 </script>
 
 <template>
-   <div class="min-h-screen">
-      <div class="absolute transform top-1/3 left-1/2 translate-x-0 translate-y-10 rotate-[12deg] shadow-md w-[450px] h-[525px] rounded-lg bg-gradient-to-br from-athAIna-red to-athAIna-yellow"></div>  
-      <div class="absolute transform top-1/3 left-1/2 -translate-x-[18.75rem] translate-y-10 rotate-[-12deg] shadow-md w-[450px] h-[525px] rounded-lg bg-gradient-to-br from-athAIna-red to-athAIna-yellow"></div>  
-      <div class="absolute transform top-1/3 left-1/2 -translate-x-[9.37rem] w-[450px] h-[600px] rounded-lg border-4 bg-athAIna-white flex flex-col p-10">
-        <div class="w-full flex flex-row justify-center items-center">
-          <img src="@/assets/athAIna.svg" alt="Logo" class="w-20" />
-        </div>
-        <h1 class="text-athAIna-violet font-semibold w-full text-center"> Create an Account </h1>
-        <div class="m-2 mb-10 bg-gradient-to-br from-athAIna-violet to-athAIna-violet rounded-[20px] h-[40px] w-full">
-          <div class="relative flex flex-row items-center">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="size-5 absolute left-3 top-1/2 transform -translate-y-1/2 text-athAIna-violet ml-2 mr-3">
-               <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
-              </svg>
-              <input v-model=email" type="email" placeholder="Email" class="text-[14px] text-athAIna-violet placeholder-athAIna-violet focus: outline-none ring- ring-athAIna-yellow w-full rounded-[15px] m-[4px] h-[32px] flex flex-row items-center pl-[50px]" />
-          </div>
-       </div>
-
-       <div class="m-2 mb-10 bg-gradient-to-br from-athAIna-violet to-athAIna-violet rounded-[20px] h-[40px] w-full">
-          <div class="relative flex flex-row items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="size-5 absolute left-3 top-1/2 transform -translate-y-1/2 text-athAIna-violet ml-2 mr-3">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
-            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-          </svg>
-              <input v-model="password" type="password" placeholder="Password" class="text-[14px] text-athAIna-violet placeholder-athAIna-violet focus: outline-none ring- ring-athAIna-yellow w-full rounded-[15px] m-[4px] h-[32px] flex flex-row items-center pl-[50px]" />
-          </div>
-       </div>
-       
-       <div class="m-2 bg-gradient-to-br from-athAIna-violet to-athAIna-violet rounded-[20px] h-[40px] w-full">
+  <div class="min-h-screen">
+    <div
+      class="absolute transform top-1/3 left-1/2 translate-x-0 translate-y-10 rotate-[12deg] shadow-md w-[450px] h-[525px] rounded-lg bg-gradient-to-br from-athAIna-red to-athAIna-yellow"
+    ></div>
+    <div
+      class="absolute transform top-1/3 left-1/2 -translate-x-[18.75rem] translate-y-10 rotate-[-12deg] shadow-md w-[450px] h-[525px] rounded-lg bg-gradient-to-br from-athAIna-red to-athAIna-yellow"
+    ></div>
+    <div
+      class="absolute transform top-1/3 left-1/2 -translate-x-[9.37rem] w-[450px] h-[600px] rounded-lg border-4 bg-athAIna-white flex flex-col p-10"
+    >
+      <div class="w-full flex flex-row justify-center items-center">
+        <img src="@/assets/athAIna.svg" alt="Logo" class="w-20" />
+      </div>
+      <h1 class="text-athAIna-violet font-semibold w-full text-center">
+        Create an Account
+      </h1>
+      <div
+        class="m-2 mb-10 bg-gradient-to-br from-athAIna-violet to-athAIna-violet rounded-[20px] h-[40px] w-full"
+      >
         <div class="relative flex flex-row items-center">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="size-5 absolute left-3 top-1/2 transform -translate-y-1/2 text-athAIna-violet ml-2 mr-3">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
-            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="2"
+            stroke="currentColor"
+            class="size-5 absolute left-3 top-1/2 transform -translate-y-1/2 text-athAIna-violet ml-2 mr-3"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"
+            />
           </svg>
-          <input v-model="confirmPassword" type="password" placeholder="Confirm Password" class="text-[14px] text-athAIna-violet placeholder-athAIna-violet focus: outline-none ring- ring-athAIna-yellow w-full rounded-[15px] m-[4px] h-[32px] flex flex-row items-center pl-[50px]" />
+          <input
+            v-model="email"
+            type="email"
+            placeholder="Email"
+            class="text-[14px] text-athAIna-violet placeholder-athAIna-violet focus: outline-none ring- ring-athAIna-yellow w-full rounded-[15px] m-[4px] h-[32px] flex flex-row items-center pl-[50px]"
+          />
         </div>
-        
-          <!-- Display error message if any -->
+      </div>
+
+      <div
+        class="m-2 mb-10 bg-gradient-to-br from-athAIna-violet to-athAIna-violet rounded-[20px] h-[40px] w-full"
+      >
+        <div class="relative flex flex-row items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="2"
+            stroke="currentColor"
+            class="size-5 absolute left-3 top-1/2 transform -translate-y-1/2 text-athAIna-violet ml-2 mr-3"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
+            />
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+            />
+          </svg>
+          <input
+            v-model="password"
+            type="password"
+            placeholder="Password"
+            class="text-[14px] text-athAIna-violet placeholder-athAIna-violet focus: outline-none ring- ring-athAIna-yellow w-full rounded-[15px] m-[4px] h-[32px] flex flex-row items-center pl-[50px]"
+          />
+        </div>
+      </div>
+
+      <div
+        class="m-2 bg-gradient-to-br from-athAIna-violet to-athAIna-violet rounded-[20px] h-[40px] w-full"
+      >
+        <div class="relative flex flex-row items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="2"
+            stroke="currentColor"
+            class="size-5 absolute left-3 top-1/2 transform -translate-y-1/2 text-athAIna-violet ml-2 mr-3"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
+            />
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+            />
+          </svg>
+          <input
+            v-model="password2"
+            type="password"
+            placeholder="Confirm Password"
+            class="text-[14px] text-athAIna-violet placeholder-athAIna-violet focus: outline-none ring- ring-athAIna-yellow w-full rounded-[15px] m-[4px] h-[32px] flex flex-row items-center pl-[50px]"
+          />
+        </div>
+
+        <!-- Display error message if any -->
         <div v-if="error" class="text-athAIna-red text-center mt-2">
           {{ error }}
         </div>
 
         <div class="flex m-10 justify-center">
-          <button class="btn w-full"> 
-            Sign Up
-          </button>
+          <button @click="createUser" class="btn w-full">Sign Up</button>
+          <p v-if="error" class="text-red-500">{{ error }}</p>
         </div>
 
-        <div class="text-center"> 
-          Already have an account? <span class="font-semibold underline"> 
-            <RouterLink to="/login"> Login </RouterLink> </span>
+        <div class="text-center">
+          Already have an account?
+          <span class="font-semibold underline">
+            <RouterLink to="/login"> Login </RouterLink>
+          </span>
         </div>
-       </div>
-      </div>  
+      </div>
     </div>
+  </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
