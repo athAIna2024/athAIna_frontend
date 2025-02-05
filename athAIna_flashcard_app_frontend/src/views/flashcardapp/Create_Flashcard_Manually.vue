02c51959aaba5a206d036ad55007086f7a05de29
eisBlume0901 <anecito2221737@mkt.ceu.edu.ph>
2025-02-05 19:51:12 +0800

    Finished polishing the Create Flashcards Manually UI

--- athAIna_flashcard_app_frontend/src/views/flashcardapp/Create_Flashcard_Manually.vue
+++ athAIna_flashcard_app_frontend/src/views/flashcardapp/Create_Flashcard_Manually.vue
@@ -26,64 +26,63 @@ const studySetName = store.studySetTitle;
       <div class="athAIna-border-outer p-1 shadow-xl">
         <div class="athAIna-border-inner py-4">
 
-          <div>
-            <div class="question mx-16 my-2">
-              <div class="border-athAIna-violet border-2 rounded-lg p-4 w-full">
-                <div class="flex flex-col gap-4">
-                  <div class="athAIna-border-outer p-1">
-                    <div class="athAIna-border-inner py-1">
-                      <textarea class="w-full rounded-lg p-4 border-0 text-athAIna-base text-athAIna-violet focus:outline-none" rows="5"></textarea>
-                    </div>
+
+          <div class="question mx-16 my-2">
+            <div class="border-athAIna-violet border-2 rounded-lg p-4 w-full">
+              <div class="flex flex-col gap-4">
+
+                <span class="text-athAIna-base text-athAIna-violet ml-1">Question</span>
+                <div class="athAIna-border-outer p-1">
+                  <div class="athAIna-border-inner py-1">
+                    <textarea class="w-full rounded-lg p-4 border-0 text-athAIna-base text-athAIna-violet placeholder-athAIna-orange focus:outline-none" rows="5" placeholder="Write your question here."></textarea>
                   </div>
-
-                  <label for="image-upload">
-                    <div class="athAIna-border-outer p-1">
-                      <div class="athAIna-border-inner py-1">
-                        <div class="flex flex-row items-center gap-2 justify-center">
-                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
-                          </svg>
-                          <span class="text-athAIna-base">+ Insert Image</span>
-                        </div>
-                        <input type="file" accept="png, jpg" id="image-upload" class="hidden" />
-                      </div>
-                    </div>
-                  </label>
-
-
-
                 </div>
 
+                <label for="image-upload">
+
+                  <div class="athAIna-border-outer p-1">
+                    <div class="athAIna-border-inner py-1">
+
+                      <div class="flex flex-row items-center gap-2 justify-center my-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                          <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+                        </svg>
+                        <span class="text-athAIna-base">+ Insert Image</span>
+                      </div>
+
+                      <input type="file" accept="png, jpg" id="image-upload" class="hidden" />
+                    </div>
+                  </div>
+                </label>
               </div>
-            </div>
 
-            <div>
-              
             </div>
           </div>
-          
-          
-          <div>
+
+          <div
+              class="w-full size-[2px] bg-gradient-to-r from-athAIna-yellow via-athAIna-orange to-athAIna-red my-4"
+          ></div>
+
+
+          <div class="answer mx-16 my-2">
+            <div class="border-athAIna-violet border-2 rounded-lg p-4 w-full">
+              <div class="flex flex-col gap-4">
+                <span class="text-athAIna-base text-athAIna-violet ml-1">Answer</span>
+                <div class="athAIna-border-outer p-1">
+                  <div class="athAIna-border-inner py-1">
+                    <textarea class="w-full rounded-lg p-4 border-0 text-athAIna-base text-athAIna-violet placeholder-athAIna-orange focus:outline-none" rows="2" placeholder="Write your answer here."></textarea>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
           </div>
 
-        </div>
+
       </div>
   </form>
 </template>
 
 <style scoped>
-::-webkit-input-placeholder {
-  text-align: center;
-}
-:-moz-placeholder {
-  /* Firefox 18- */
-  text-align: center;
-}
-::-moz-placeholder {
-  /* Firefox 19+ */
-  text-align: center;
-}
-:-ms-input-placeholder {
-  text-align: center;
-}
 </style>
