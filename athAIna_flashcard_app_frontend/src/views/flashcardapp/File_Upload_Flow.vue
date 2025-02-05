5f99f5bcaaee7a4685dd277ad777ba221a2c1ac6
— <alekzene.cs@gmail.com>
2025-02-05 19:07:43 +0800

    state mgmt: create file upload flow

--- athAIna_flashcard_app_frontend/src/views/flashcardapp/File_Upload_Flow.vue
+++ athAIna_flashcard_app_frontend/src/views/flashcardapp/File_Upload_Flow.vue
@@ -1,9 +1,15 @@
 <script setup>
+import { ref, provide } from "vue";
+import Upload_File from "@/components/Upload_File.vue";
+import File_Ready from "@/components/File_Ready.vue";
 
+const uploadedFile = ref(null);
+provide("uploadedFile", uploadedFile);
 </script>
 
 <template>
-  $END$
+  <Upload_File />
+  <File_Ready />
 </template>
 
 <style scoped>
