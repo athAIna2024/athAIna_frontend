db09db1cf242c9fde1d017153cadfd63e82f6b08
casp3r2 <73689043+casp3r2@users.noreply.github.com>
2025-02-18 18:42:42 +0800

    Frontend Update
    View password (eye icon) :
    Applied it to
    - Sign up
    - Forgot
    - Change
    - Login

--- athAIna_flashcard_app_frontend/src/views/accountapp/Signup.vue
+++ athAIna_flashcard_app_frontend/src/views/accountapp/Signup.vue
@@ -22,6 +22,17 @@ const closeOTP = () => {
   isOTPVisible.value = false;
 };
 
+const showPassword = ref(false);
+const showPassword2 = ref(false);
+
+const togglePassword = () => {
+  showPassword.value = !showPassword.value;
+};
+
+const togglePassword2 = () => {
+  showPassword2.value = !showPassword2.value;
+};
+
 const errors = reactive({
   email: "",
   password: "",
@@ -93,6 +104,7 @@ const createUser = async () => {
       <h1 class="text-athAIna-violet font-semibold w-full text-center">
         Create an Account
       </h1>
+      <!-- Email Field -->
       <div
         class="m-2 mb-10 bg-gradient-to-br from-athAIna-violet to-athAIna-violet rounded-[20px] h-[40px] w-full"
       >
@@ -123,6 +135,7 @@ const createUser = async () => {
         </div>
       </div>
 
+      <!--Password Field -->
       <div
         class="m-2 mb-10 bg-gradient-to-br from-athAIna-violet to-athAIna-violet rounded-[20px] h-[40px] w-full"
       >
@@ -138,20 +151,40 @@ const createUser = async () => {
             <path
               stroke-linecap="round"
               stroke-linejoin="round"
-              d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
-            />
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
               d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
             />
           </svg>
           <input
             v-model="password"
-            type="password"
+            :type="showPassword ? 'text' : 'password'"
             placeholder="Password"
             class="text-[14px] text-athAIna-violet placeholder-athAIna-violet focus: outline-none ring- ring-athAIna-yellow w-full rounded-[15px] m-[4px] h-[32px] flex flex-row items-center pl-[50px]"
           />
+          <button
+            v-if="password"
+            @click="togglePassword"
+            type="button"
+            class="absolute right-3 top-1/2 transform -translate-y-1/2"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="2"
+              stroke="currentColor"
+              class="size-5 text-athAIna-violet"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                :d="
+                  showPassword
+                    ? 'M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z M15 12a3 3 0 11-6 0 3 3 0 016 0z'
+                    : 'M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88'
+                "
+              />
+            </svg>
+          </button>
         </div>
         <div
           v-if="errors.password[0]"
@@ -160,6 +193,8 @@ const createUser = async () => {
           {{ errors.password[0] }}
         </div>
       </div>
+
+      <!-- Confirm Password Field -->
 
       <div
         class="m-2 bg-gradient-to-br from-athAIna-violet to-athAIna-violet rounded-[20px] h-[40px] w-full"
@@ -176,20 +211,40 @@ const createUser = async () => {
             <path
               stroke-linecap="round"
               stroke-linejoin="round"
-              d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
-            />
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
               d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
             />
           </svg>
           <input
             v-model="password2"
-            type="password"
+            :type="showPassword2 ? 'text' : 'password'"
             placeholder="Confirm Password"
             class="text-[14px] text-athAIna-violet placeholder-athAIna-violet focus: outline-none ring- ring-athAIna-yellow w-full rounded-[15px] m-[4px] h-[32px] flex flex-row items-center pl-[50px]"
           />
+          <button
+            v-if="password2"
+            @click="togglePassword2"
+            type="button"
+            class="absolute right-3 top-1/2 transform -translate-y-1/2"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="2"
+              stroke="currentColor"
+              class="size-5 text-athAIna-violet"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                :d="
+                  showPassword2
+                    ? 'M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z M15 12a3 3 0 11-6 0 3 3 0 016 0z'
+                    : 'M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88'
+                "
+              />
+            </svg>
+          </button>
         </div>
         <div
           v-if="errors.password2[0]"
