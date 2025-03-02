14f999977e6d6ba2d881618dd60778dd01db1cf2
eisBlume0901 <anecito2221737@mkt.ceu.edu.ph>
2025-03-02 23:04:04 +0800

    Implement temporary solution for backtracking to show previous answer instead of next question

--- athAIna_flashcard_app_frontend/src/components/Test_Mode_Flashcard.vue
+++ athAIna_flashcard_app_frontend/src/components/Test_Mode_Flashcard.vue
@@ -2,6 +2,7 @@
 
 import { ref } from 'vue';
 import { watch } from 'vue';
+
 import { defineProps } from 'vue';
 import { useTestModeStore} from "../../stores/testModeStore.js";
 
@@ -24,60 +25,72 @@ const props = defineProps({
   },
 });
 
-watch(() => props.question, (newQuestion) => {
-  showQuestion.value = true;
-  showAnswer.value = false;
-});
 
 const submitAnswer = () => {
-  showQuestion.value = false;
-  showAnswer.value = true;
-  setTimeout(() => {
-    showAnswer.value = false;
-    if (testModeStore.currentQuestionIndex < testModeStore.numberOfQuestions) {
-      const increment = testModeStore.currentQuestionIndex + 1;
-      testModeStore.setCurrentQuestionIndex(increment);
-
-      // location.reload(); // Temporary solution but not recommended since it refreshes the page
-    }
-  }, 1000); // change to 60000 (1000 for testing)
+  displayAnswer();
 };
 
 
+const displayAnswer = () => {
+  showAnswer.value = true;
+  showQuestion.value = false;
+
+  setTimeout(() => {
+    transitionToNext();
+  }, 1000); // Adjust the delay as needed
+
+};
+
+const transitionToNext = () => {
+  showAnswer.value = false;
+  if (!showAnswer.value && testModeStore.currentQuestionIndex < testModeStore.numberOfQuestions) {
+    const increment = testModeStore.currentQuestionIndex + 1;
+    testModeStore.setCurrentQuestionIndex(increment);
+    showQuestion.value = true;
+  }
+};
+
+watch(() => showQuestion.value, (newValue) => {
+  if (newValue) {
+    console.log("Question is now being shown");
+  } else {
+    console.log("Question is now hidden");
+  }
+});
 </script>
 
 <template>
 
-  <div v-if="showQuestion" class="athAIna-border-outer p-1 my-4">
-    <div class="athAIna-border-inner">
-      <div class="text-athAIna-violet m-auto flex items-center justify-center h-96">
-        {{  props.question }}
-      </div>
-      <div class="h-20">
-        <div class="px-12 py-4 flex flex-row gap-10 justify-between ">
-          <div class="athAIna-border-outer p-1 rounded-[20px] h-[40px]">
-            <div class="athAIna-border-inner rounded-[15px] relative flex flex-row items-center">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="size-5 absolute left-3 top-1/2 transform -translate-y-1/2 text-athAIna-orange ml-2 mr-3">
-                <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-              </svg>
-              <input type="text" v-model="learner_answer" placeholder="Enter Answer" class="text-[14px] text-athAIna-orange placeholder-athAIna-orange focus: outline-none ring- ring-athAIna-yellow w-full rounded-[15px] m-[4px] h-[32px] flex flex-row items-center pl-[50px]">
-            </div>
-          </div>
-
-          <button class="btn w-48" @click="submitAnswer"> Send </button>
+<!--  <Transition name="fade">-->
+    <div v-if="showQuestion" class="athAIna-border-outer p-1 my-4">
+      <div class="athAIna-border-inner">
+        <div class="text-athAIna-violet m-auto flex items-center justify-center h-96">
+          {{ props.question }}
         </div>
+        <div class="h-20">
+          <div class="px-12 py-4 flex flex-row gap-10 justify-between ">
+            <div class="athAIna-border-outer p-1 rounded-[20px] h-[40px]">
+              <div class="athAIna-border-inner rounded-[15px] relative flex flex-row items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="size-5 absolute left-3 top-1/2 transform -translate-y-1/2 text-athAIna-orange ml-2 mr-3">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                </svg>
+                <input type="text" v-model="learner_answer" placeholder="Enter Answer" class="text-[14px] text-athAIna-orange placeholder-athAIna-orange focus: outline-none ring- ring-athAIna-yellow w-full rounded-[15px] m-[4px] h-[32px] flex flex-row items-center pl-[50px]">
+              </div>
+            </div>
 
-
-       </div>
+            <button class="btn w-48" @click="submitAnswer"> Send </button>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
+<!--  </Transition>-->
 
   <Transition name="slide-fade">
     <div v-if="showAnswer" class="athAIna-border-outer p-1 my-4">
       <div class="athAIna-border-inner">
         <div class="px-10 py-8 flex items-center">
           <span class="text-athAIna-base">
-            {{ props.question}}
+            {{ props.question }}
           </span>
         </div>
 
@@ -85,11 +98,10 @@ const submitAnswer = () => {
           <span class="text-athAIna-sm">
             Your answer
           </span>
-            <span class="text-athAIna-green text-athAIna-base">
+          <span class="text-athAIna-green text-athAIna-base">
             insert learner answer
           </span>
         </div>
-
 
         <div class="m-6">
           <div class="athAIna-border-outer p-0.5">
@@ -104,18 +116,10 @@ const submitAnswer = () => {
             {{ props.answer }}
           </span>
         </div>
-
       </div>
     </div>
-
-
-</Transition>
-
-
-
-
+  </Transition>
 </template>
-
 <style scoped>
 
 .slide-fade-enter-active {
@@ -123,7 +127,7 @@ const submitAnswer = () => {
 }
 
 .slide-fade-leave-active {
-  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+  transition: all 0.3s cubic-bezier(1, 0.5, 0.8, 1);
 }
 
 .slide-fade-enter-from,
