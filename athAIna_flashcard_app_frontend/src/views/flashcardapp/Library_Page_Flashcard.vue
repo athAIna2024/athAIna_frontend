d41d0606d22835aabe3973eba7eb4824ecf96c92
netzumin <115604150+3aarl@users.noreply.github.com>
2025-03-08 15:33:28 +0800

    library review mode button functions

--- athAIna_flashcard_app_frontend/src/views/flashcardapp/Library_Page_Flashcard.vue
+++ athAIna_flashcard_app_frontend/src/views/flashcardapp/Library_Page_Flashcard.vue
@@ -1,19 +1,19 @@
 <script setup>
-import {ref} from 'vue';
-import {computed} from "vue";
-import {onMounted} from "vue";
+import { ref } from "vue";
+import { computed } from "vue";
+import { onMounted } from "vue";
 import Flashcard_Search_Bar from "@/components/Search_Bar.vue";
 import Flashcard_Card from "@/components/Flashcard_Card.vue";
 import AI_Flashcard from "@/views/flashcardapp/Generate_Flashcard_with_AI.vue";
 import Pagination from "@/components/Pagination.vue";
 
-import { useRouter } from 'vue-router';
-import { useTestModeStore} from "../../../stores/testModeStore.js";
-import {useStudysetStore} from "../../../stores/studySetStore.js";
-import {useFlashcardStore} from "../../../stores/flashcardStore.js";
+import { useRouter } from "vue-router";
+import { useTestModeStore } from "../../../stores/testModeStore.js";
+import { useStudysetStore } from "../../../stores/studySetStore.js";
+import { useFlashcardStore } from "../../../stores/flashcardStore.js";
 import flashcardsDB from "@/views/flashcardapp/dexie.js";
 
-import axios from '@/axios';
+import axios from "@/axios";
 const testModeStore = useTestModeStore();
 const studySetStore = useStudysetStore();
 const flashcardStore = useFlashcardStore();
@@ -23,7 +23,6 @@ const studySetId = Number(studySetStore.studySetId);
 const isSuccessful = ref(false);
 const message = ref("");
 const flashcard_db = ref([]);
-
 
 const isSuccessful_test = ref(false);
 const message_test = ref("");
@@ -48,11 +47,10 @@ const dropdownOptions = ref({
   SCI: "Science",
   SOC_SCI: "Social Sciences",
   TECH: "Technology",
-  WRIT_LIT: "Writing and Literature"
+  WRIT_LIT: "Writing and Literature",
 });
 
-
-const input = ref('');
+const input = ref("");
 const currentPage = ref(1);
 
 const modals = ref({
@@ -63,7 +61,6 @@ const modals = ref({
 });
 
 const isAIFlashcardVisible = ref(false);
-
 
 const toggleModal = (modalName) => {
   modals.value[modalName] = !modals.value[modalName];
@@ -87,16 +84,15 @@ const errorMessage = ref({
   numberOfQuestions: null,
 });
 
-const randomizeTestUrl = '/test/randomize/'
+const randomizeTestUrl = "/test/randomize/";
 const randomizeTestModeFlashcards = async () => {
-
   const numberOfQuestions = Number(testModeStore.numberOfQuestions);
   try {
-    const response = await axios.get(`${randomizeTestUrl}`,{
+    const response = await axios.get(`${randomizeTestUrl}`, {
       params: {
         studyset_id: studySetId,
-        number_of_questions: numberOfQuestions
-      }
+        number_of_questions: numberOfQuestions,
+      },
     });
 
     isSuccessful_test.value = response.data.successful;
@@ -105,34 +101,52 @@ const randomizeTestModeFlashcards = async () => {
     if (isSuccessful_test.value) {
       testModeStore.setTestModeQuestions(response.data.flashcard_ids);
     }
-
-  } catch (error) {
-
-  }
+  } catch (error) {}
 };
 const redirectToTestMode = () => {
-
-  if (numberOfQuestions.value <= 0 || numberOfQuestions.value === null || isNaN(numberOfQuestions.value)) {
+  if (
+    numberOfQuestions.value <= 0 ||
+    numberOfQuestions.value === null ||
+    isNaN(numberOfQuestions.value)
+  ) {
     errors.value.numberOfQuestions = true;
-    errorMessage.value.numberOfQuestions = "You cannot test yourself with empty questions."
+    errorMessage.value.numberOfQuestions =
+      "You cannot test yourself with empty questions.";
   } else if (numberOfQuestions.value > flashcardCounts.value) {
     errors.value.numberOfQuestions = true;
-    errorMessage.value.numberOfQuestions = "It is not possible to take more questions beyond the available flashcards."
+    errorMessage.value.numberOfQuestions =
+      "It is not possible to take more questions beyond the available flashcards.";
   } else {
     errors.value.numberOfQuestions = false;
     errorMessage.value.numberOfQuestions = null;
     testModeStore.setNumberOfQuestions(numberOfQuestions.value);
-    toggleModal('takeTest');
+    toggleModal("takeTest");
 
     randomizeTestModeFlashcards();
-    router.push({ name: 'Test_Mode', params: { studySetTitle: studySetTitle, studySetId: studySetId } });
+    router.push({
+      name: "Test_Mode",
+      params: { studySetTitle: studySetTitle, studySetId: studySetId },
+    });
   }
 };
 
+const redirectToReviewMode = async () => {
+  const flashcards = await flashcardsDB.flashcards
+    .where("studyset_id")
+    .equals(studySetId)
+    .toArray();
+  const filteredFlashcards = flashcards.filter((f) => f.id !== studySetId);
+  const randomFlashcard =
+    filteredFlashcards[Math.floor(Math.random() * filteredFlashcards.length)];
+  router.push(`/${studySetTitle}/${studySetId}/review/${randomFlashcard.id}`);
+};
 
 const fetchFlashcardsFromDb = async () => {
   try {
-    flashcard_db.value = await flashcardsDB.flashcards.where('studyset_id').equals(studySetId).toArray();
+    flashcard_db.value = await flashcardsDB.flashcards
+      .where("studyset_id")
+      .equals(studySetId)
+      .toArray();
 
     if (flashcard_db.value.length === 0) {
       isSuccessful.value = false;
@@ -141,12 +155,11 @@ const fetchFlashcardsFromDb = async () => {
       isSuccessful.value = true;
       message.value = "Flashcards retrieved successfully.";
     }
-
   } catch (error) {
     isSuccessful.value = false;
     message.value = "An error occurred. Please try again later.";
   }
-}
+};
 
 const currentFlashcards = computed(() => {
   const startIndex = (currentPage.value - 1) * itemsPerPage;
@@ -160,49 +173,88 @@ onMounted(() => {
 });
 
 const navigateToLibraryPage = () => {
-  router.push({ name: 'Library_Page_Studyset' });
+  router.push({ name: "Library_Page_Studyset" });
 };
 </script>
 
 <template>
-{{ studySetId}}
+  {{ studySetId }}
   {{ studySetTitle }}
   <div class="m-4">
     <div class="athAIna-border-outer p-1 shadow-xl">
       <div class="athAIna-border-inner py-4">
-
         <div class="flex flex-col m-10">
-          <div class="text-athAIna-lg text-center flex flex-row justify-between space-x-20 items-center">
-
-
+          <div
+            class="text-athAIna-lg text-center flex flex-row justify-between space-x-20 items-center"
+          >
             <div class="flex flex-row space-x-6 items-center">
               <button @click="navigateToLibraryPage">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="size-6">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  class="size-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+                  />
                 </svg>
               </button>
-              <h1 class="text-athAIna-violet font-semibold flex"> {{ studySetTitle }} </h1>
+              <h1 class="text-athAIna-violet font-semibold flex">
+                {{ studySetTitle }}
+              </h1>
             </div>
 
             <div class="flex flex-row justify-between space-x-6 items-center">
               <Flashcard_Search_Bar v-model="input" />
-              <button class="relative btn w-60 text-[16px] font-semibold" @click="toggleModal('learningMode')"> Learning Mode </button>
-              <div v-if="modals.learningMode" class="absolute top-[230px] right-[315px] h-[150px] w-[235px] border-athAIna-orange border-[4px] rounded-3xl bg-athAIna-white flex flex-col justify-between p-5">
-                <button class="text-athAIna-base border-athAIna-orange border-[3.5px] py-[10px] px-[30px] rounded-2xl text-sm">
-                  <router-link to="review/1"> Review Mode </router-link>
+              <button
+                class="relative btn w-60 text-[16px] font-semibold"
+                @click="toggleModal('learningMode')"
+              >
+                Learning Mode
+              </button>
+              <div
+                v-if="modals.learningMode"
+                class="absolute top-[230px] right-[315px] h-[150px] w-[235px] border-athAIna-orange border-[4px] rounded-3xl bg-athAIna-white flex flex-col justify-between p-5"
+              >
+                <button
+                  @click="redirectToReviewMode"
+                  class="text-athAIna-base border-athAIna-orange border-[3.5px] py-[10px] px-[30px] rounded-2xl text-sm"
+                >
+                  Review Mode
                 </button>
-                <button class="text-athAIna-base bg-athAIna-orange py-[10px] px-[30px] rounded-2xl text-sm text-athAIna-white" @click="toggleModal('takeTest')">
+                <button
+                  class="text-athAIna-base bg-athAIna-orange py-[10px] px-[30px] rounded-2xl text-sm text-athAIna-white"
+                  @click="toggleModal('takeTest')"
+                >
                   Test Mode
                 </button>
               </div>
-              <button class="relative btn w-60 text-[16px] font-semibold" @click="toggleModal('addFlashcard')"> Add Flashcard </button>
-              <div v-if="modals.addFlashcard" class="absolute top-[230px] right-[47px] h-[150px] w-[240px] border-athAIna-orange border-[4px] rounded-3xl bg-athAIna-white flex flex-col justify-between p-5">
-                <router-link :to="{name: 'Create_Flashcard_Manually'}">
-                  <button class="text-athAIna-base border-athAIna-orange border-[3.5px] py-[10px] px-[30px] rounded-2xl text-sm">
-                     Create Manually
+              <button
+                class="relative btn w-60 text-[16px] font-semibold"
+                @click="toggleModal('addFlashcard')"
+              >
+                Add Flashcard
+              </button>
+              <div
+                v-if="modals.addFlashcard"
+                class="absolute top-[230px] right-[47px] h-[150px] w-[240px] border-athAIna-orange border-[4px] rounded-3xl bg-athAIna-white flex flex-col justify-between p-5"
+              >
+                <router-link :to="{ name: 'Create_Flashcard_Manually' }">
+                  <button
+                    class="text-athAIna-base border-athAIna-orange border-[3.5px] py-[10px] px-[30px] rounded-2xl text-sm"
+                  >
+                    Create Manually
                   </button>
                 </router-link>
-                <button @click="openAI_Flashcard" class="text-athAIna-base bg-athAIna-orange py-[10px] px-[30px] rounded-2xl text-sm text-athAIna-white">
+                <button
+                  @click="openAI_Flashcard"
+                  class="text-athAIna-base bg-athAIna-orange py-[10px] px-[30px] rounded-2xl text-sm text-athAIna-white"
+                >
                   Generate with AI
                 </button>
               </div>
@@ -210,8 +262,9 @@ const navigateToLibraryPage = () => {
           </div>
 
           <div class="mt-4 mb-6">
-            <div class="w-full size-[1px] bg-gradient-to-r from-athAIna-yellow via-athAIna-orange to-athAIna-red mt-[12px]">
-            </div>
+            <div
+              class="w-full size-[1px] bg-gradient-to-r from-athAIna-yellow via-athAIna-orange to-athAIna-red mt-[12px]"
+            ></div>
           </div>
 
           <div class="flex flex-row gap-2">
@@ -224,12 +277,18 @@ const navigateToLibraryPage = () => {
           </div>
 
           <div class="grid grid-cols-3 gap-12 mt-10 mb-12">
-            <li class="list-none" v-for="(flashcard, index) in flashcardStore.searchResults.length ? flashcardStore.searchResults : currentFlashcards" :key="index">
+            <li
+              class="list-none"
+              v-for="(flashcard, index) in flashcardStore.searchResults.length
+                ? flashcardStore.searchResults
+                : currentFlashcards"
+              :key="index"
+            >
               <Flashcard_Card
-                  :flashcardId="flashcard.id"
-                  :question="flashcard.question"
-                  :answer="flashcard.answer"
-                  :image="flashcard.image"
+                :flashcardId="flashcard.id"
+                :question="flashcard.question"
+                :answer="flashcard.answer"
+                :image="flashcard.image"
               />
             </li>
             <div class="item error" v-if="!isSuccessful">
@@ -242,36 +301,59 @@ const navigateToLibraryPage = () => {
 
           <div class="flex justify-center">
             <Pagination
-                :total-items="flashcardCounts"
-                :items-per-page="itemsPerPage"
-                :current-page="currentPage"
-                @update:currentPage="currentPage = $event"
+              :total-items="flashcardCounts"
+              :items-per-page="itemsPerPage"
+              :current-page="currentPage"
+              @update:currentPage="currentPage = $event"
             />
           </div>
         </div>
       </div>
-
     </div>
-
   </div>
 
   <AI_Flashcard :is-visible="isAIFlashcardVisible" @close="closeAI_Flashcard" />
-  <div v-if="modals.takeTest" class="fixed inset-0 flex items-center justify-center bg-[rgba(0,0,0,0.5)] bg-opacity-50 z-40">
+  <div
+    v-if="modals.takeTest"
+    class="fixed inset-0 flex items-center justify-center bg-[rgba(0,0,0,0.5)] bg-opacity-50 z-40"
+  >
     <div class="athAIna-border-outer p-1 flex flex-col w-[550px]">
       <div class="athAIna-border-inner p-4 text-center">
         <div class="flex flex-col p-2">
           <button @click="toggleModal('takeTest')" class="flex flex-start">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="size-6">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="2"
+              stroke="currentColor"
+              class="size-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+              />
             </svg>
           </button>
 
           <div class="flex flex-col space-y-4 p-6 items-center justify-between">
-            <h1 class="text-athAIna-lg font-medium">Number of Questions to be Taken</h1>
+            <h1 class="text-athAIna-lg font-medium">
+              Number of Questions to be Taken
+            </h1>
             <div class="px-2">
-              <input type="number" v-model="numberOfQuestions" class="text-athAIna-base font-medium text-athAIna-violet w-52 placeholder-athAIna-violet rounded-xl border-1 ring-2 ring-athAIna-violet border-athAIna-violet pl-4 focus: outline-none" />
+              <input
+                type="number"
+                v-model="numberOfQuestions"
+                class="text-athAIna-base font-medium text-athAIna-violet w-52 placeholder-athAIna-violet rounded-xl border-1 ring-2 ring-athAIna-violet border-athAIna-violet pl-4 focus: outline-none"
+              />
             </div>
-            <div :style="{ visibility: errors.numberOfQuestions ? 'visible' : 'hidden' }" class="text-athAIna-red text-athAIna-base">
+            <div
+              :style="{
+                visibility: errors.numberOfQuestions ? 'visible' : 'hidden',
+              }"
+              class="text-athAIna-red text-athAIna-base"
+            >
               {{ errorMessage.numberOfQuestions }}
             </div>
 
@@ -280,11 +362,7 @@ const navigateToLibraryPage = () => {
             </button>
           </div>
         </div>
-
-
       </div>
     </div>
   </div>
-
 </template>
-
