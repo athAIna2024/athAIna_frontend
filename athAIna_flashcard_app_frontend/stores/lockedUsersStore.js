e874d8f543925a69e3da8134f3437ef3b301cc15
netzumin <115604150+3aarl@users.noreply.github.com>
2025-03-17 18:50:39 +0800

    very buggy update to locking only specific users

---
+++ athAIna_flashcard_app_frontend/stores/lockedUsersStore.js
@@ -0,0 +1,47 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+export const useLockedUsersStore = defineStore("lockedUsers", () => {
+  const lockedUsers = ref([]);
+
+  const setLockedUsers = (userId, lockoutEndTime) => {
+    lockedUsers.value[userId] = lockoutEndTime;
+    localStorage.setItem("lockedUsers", JSON.stringify(lockedUsers.value));
+  };
+
+  const removeLockedUser = (userId) => {
+    delete lockedUsers.value[userId];
+    localStorage.setItem("lockedUsers", JSON.stringify(lockedUsers.value));
+  };
+
+  const isUserLocked = (userId) => {
+    const lockoutEndTime = lockedUsers.value[userId];
+    if (lockoutEndTime) {
+      const currentTime = Date.now();
+      if (currentTime < lockoutEndTime) {
+        return true;
+      } else {
+        removeLockedUser(userId);
+      }
+    }
+    return false;
+  };
+
+  const loadLockedUsers = () => {
+    const storedLockedUsers = JSON.parse(localStorage.getItem("lockedUsers"));
+    if (storedLockedUsers) {
+      lockedUsers.value = storedLockedUsers;
+    }
+  };
+
+  loadLockedUsers();
+
+  persist: true;
+
+  return {
+    lockedUsers,
+    setLockedUsers,
+    removeLockedUser,
+    isUserLocked,
+  };
+});
