54ddf6bbb946e893e0bd619d49415a8bfce88586
eisBlume0901 <anecito2221737@mkt.ceu.edu.ph>
2025-03-21 09:59:39 +0800

    Change placeholder from Search Study Set to Search Flashcard

--- athAIna_flashcard_app_frontend/src/components/Search_Bar.vue
+++ athAIna_flashcard_app_frontend/src/components/Search_Bar.vue
@@ -36,6 +36,8 @@ const searchFlashcards = async (query) => {
       )
       .toArray();
 };
+
+
 </script>
 
 <template>
@@ -44,7 +46,7 @@ const searchFlashcards = async (query) => {
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="size-5 absolute left-3 top-1/2 transform -translate-y-1/2 text-athAIna-orange ml-2 mr-3">
         <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
       </svg>
-      <input type="search" :value="modelValue" @input="updateValue" @keydown="handleKeyPress" placeholder="Search Study Set" class="text-[14px] text-athAIna-orange placeholder-athAIna-orange focus: outline-none ring- ring-athAIna-yellow w-full rounded-[15px] m-[4px] h-[32px] flex flex-row items-center pl-[50px]">
+      <input type="search" :value="modelValue" @input="updateValue" @keydown="handleKeyPress" placeholder="Search Flashcard" class="text-[14px] text-athAIna-orange placeholder-athAIna-orange focus: outline-none ring- ring-athAIna-yellow w-full rounded-[15px] m-[4px] h-[32px] flex flex-row items-center pl-[50px]">
     </div>
   </div>
 </template>
