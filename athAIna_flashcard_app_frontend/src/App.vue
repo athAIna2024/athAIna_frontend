ac04c2e585eb9d207986c907e36d599c6f81353a
netzumin <115604150+3aarl@users.noreply.github.com>
2025-03-24 12:48:12 +0800

    reactive navbar

--- athAIna_flashcard_app_frontend/src/App.vue
+++ athAIna_flashcard_app_frontend/src/App.vue
@@ -3,9 +3,10 @@ import { useAuthStore } from "../stores/authStore.js";
 import Guest_Navbar from "@/components/Guest_Navbar.vue";
 import Learner_Navbar from "@/components/Learner_Navbar.vue";
 import Footer_Navbar from "@/components/Footer_Navbar.vue";
+import { computed } from "vue";
 
 const authStore = useAuthStore();
-const isLoggedIn = authStore.isLoggedIn;
+const isLoggedIn = computed(() => authStore.isLoggedIn);
 </script>
 
 <template class="flex flex-col min-h-screen">
