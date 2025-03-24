580ff0a189d1ce2e3b2f4e5aeee3004e95795353
netzumin <115604150+3aarl@users.noreply.github.com>
2025-03-24 12:45:39 +0800

    Adjusted isLoggedin navbars

--- athAIna_flashcard_app_frontend/src/App.vue
+++ athAIna_flashcard_app_frontend/src/App.vue
@@ -1,21 +1,21 @@
 <script setup>
 import { useAuthStore } from "../stores/authStore.js";
-import Guest_Navbar from '@/components/Guest_Navbar.vue';
+import Guest_Navbar from "@/components/Guest_Navbar.vue";
 import Learner_Navbar from "@/components/Learner_Navbar.vue";
 import Footer_Navbar from "@/components/Footer_Navbar.vue";
 
 const authStore = useAuthStore();
+const isLoggedIn = authStore.isLoggedIn;
 </script>
 
 <template class="flex flex-col min-h-screen">
   <header>
     <Guest_Navbar v-if="!isLoggedIn" />
-    <Learner_Navbar v-if="isLoggedIn" /> 
+    <Learner_Navbar v-if="isLoggedIn" />
   </header>
 
   <main>
     <router-view />
-
   </main>
 
   <footer>
@@ -23,6 +23,4 @@ const authStore = useAuthStore();
   </footer>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
