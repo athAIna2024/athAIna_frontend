5911c720635018b1041bf3cad0616a8905e1716b
casp3r2 <73689043+casp3r2@users.noreply.github.com>
2025-04-04 19:52:31 +0800

    Fixed Port back to 8009

--- athAIna_flashcard_app_frontend/src/axiosConfig.js
+++ athAIna_flashcard_app_frontend/src/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "http://localhost:8009",
   withCredentials: true,
 });
 
