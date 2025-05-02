995b05a3dfd9d86ca224070a3dcd09d2b0c29adf
eisBlume0901 <anecito2221737@mkt.ceu.edu.ph>
2025-05-02 19:27:27 +0800

    Adjusted padding size of showing answer to the learner to be the same with showing the question

--- athAIna_flashcard_app_frontend/src/components/Test_Mode_Flashcard.vue
+++ athAIna_flashcard_app_frontend/src/components/Test_Mode_Flashcard.vue
@@ -282,7 +282,7 @@ const isValidImage = computed(() => {
 
 
         <div class="flex lg:flex-row items-center justify-center p-10 md:flex-col-reverse sm:flex-col-reverse">
-          <div class="text-athAIna-violet m-auto flex items-center justify-center">
+          <div class="text-athAIna-violet m-auto flex items-center justify-center h-96">
             {{ props.question }}
           </div>
           <div v-if="isValidImage">
@@ -300,10 +300,7 @@ const isValidImage = computed(() => {
           <div class="px-12 py-4 flex flex-row gap-10 justify-between ">
             <div class="athAIna-border-outer p-1 rounded-[20px] h-[40px]">
               <div class="athAIna-border-inner rounded-[15px] relative flex flex-row items-center">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="size-5 absolute left-3 top-1/2 transform -translate-y-1/2 text-athAIna-orange ml-2 mr-3">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-                </svg>
-                <input type="text" v-model="learner_answer" placeholder="Enter Answer" class="text-[14px] text-athAIna-orange placeholder-athAIna-orange focus: outline-none ring- ring-athAIna-yellow w-full rounded-[15px] m-[4px] h-[32px] flex flex-row items-center pl-[50px]">
+                <input type="text" v-model="learner_answer" placeholder="Enter Answer" class="text-[14px] text-athAIna-orange placeholder-athAIna-orange focus: outline-none ring- ring-athAIna-yellow w-full rounded-[15px] m-[4px] h-[32px] flex flex-row items-center pl-[20px]">
               </div>
             </div>
 
@@ -316,40 +313,41 @@ const isValidImage = computed(() => {
 
   <Transition name="slide-fade">
     <div v-if="showAnswer" class="athAIna-border-outer p-1 my-4">
-      <div class="athAIna-border-inner">
-        <div class="px-10 py-8 flex items-center">
+      <div class="athAIna-border-inner p-6">
+        <div class="">
+          <div class="px-10 py-8 flex items-center">
           <span class="text-athAIna-base">
             {{ props.question }}
           </span>
-        </div>
+          </div>
 
-        <div class="flex flex-col px-10 py-8 gap-y-4">
+          <div class="flex flex-col px-10 py-8 gap-y-4">
           <span class="text-athAIna-sm">
             Your answer
           </span>
-          <span :class="[answerClass, 'text-athAIna-base']">
+            <span :class="[answerClass, 'text-athAIna-base']">
             {{ learner_answer ? learner_answer : 'You did not provide an answer.' }}
           </span>
-        </div>
-
-        <div class="m-6">
-          <div class="athAIna-border-outer p-0.5">
           </div>
-        </div>
 
-        <div class="flex flex-col px-10 py-8 gap-y-4">
+          <div class="m-6">
+            <div class="athAIna-border-outer p-0.5">
+            </div>
+          </div>
+
+          <div class="flex flex-col px-10 py-8 gap-y-4">
           <span class="text-athAIna-sm">
             Correct answer
           </span>
-          <span class="text-athAIna-green text-athAIna-base">
+            <span class="text-athAIna-green text-athAIna-base">
             {{ props.answer }}
           </span>
-        </div>
+          </div>
 
-        <div class="px-10 py-8 flex items-center justify-end">
-          <button class="btn w-48" @click="transitionToNext">Next</button>
+          <div class="px-10 py-8 flex items-center justify-end">
+            <button class="btn w-48" @click="transitionToNext">Next</button>
+          </div>
         </div>
-
       </div>
     </div>
   </Transition>
