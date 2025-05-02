c5aad931d849d8a3dc881161a1b83ab1ac279f99
eisBlume0901 <anecito2221737@mkt.ceu.edu.ph>
2025-05-03 07:56:01 +0800

    Refactored Studyset Card to navigate to the Library Page Flashcard on card click. Added .stop modifier to delete and update icon buttons to prevent navigation trigger.

--- athAIna_flashcard_app_frontend/src/components/Studyset_Card.vue
+++ athAIna_flashcard_app_frontend/src/components/Studyset_Card.vue
@@ -190,9 +190,11 @@ onMounted(async () => {
 <!--  />-->
 
   <div class="p-[5px] shadow-md bg-gradient-to-br rounded-[20px] from-athAIna-yellow via-athAIna-orange to-athAIna-red">
-    <div @click="navigateToLibraryPageFlashcard"
-        class="flex flex-col h-40 bg-athAIna-white rounded-[15px] p-[15px] hover:cursor-pointer">
-        <div class="text-[20px] font-semibold"> {{ title }}</div>
+    <div class="flex flex-col h-40 bg-athAIna-white rounded-[15px] p-[15px] hover:cursor-pointer"
+         @click="navigateToLibraryPageFlashcard"
+    >
+
+      <div class="text-[20px] font-semibold"> {{ title }}</div>
       <div class="text-[16px] text-athAIna-orange"> {{ subject }} </div>
       <div class="text-[14px] mt-[12px] h-12">
       <span v-if="description !== 'null' && description.length < 50">
@@ -202,16 +204,17 @@ onMounted(async () => {
         {{ description.substring(0, 50) + "..." }}
       </span>
       </div>
+
       <div class="flex flex-row justify-between mt-[18px]">
         <div>
           <span class="font-bold"> {{ flashcardCount }} </span>
           <span>{{ flashcardCount > 1 ? ' flashcards' : ' flashcard' }}</span>
         </div>
         <div class="flex flex-row">
-          <svg @click="openUpdateModal" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="hover:cursor-pointer size-6">
+          <svg @click.stop="openUpdateModal" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="hover:cursor-pointer size-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
           </svg>
-          <svg @click="openDeleteModal" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 hover:cursor-pointer">
+          <svg @click.stop="openDeleteModal" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 hover:cursor-pointer">
             <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
           </svg>
         </div>
